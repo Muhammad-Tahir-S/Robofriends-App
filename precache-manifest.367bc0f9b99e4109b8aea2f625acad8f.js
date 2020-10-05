48144475d15dc2ada9d9adb6f794ed2645b4023c
Muhammad-Tahir Sanuth <mtz5prif@gmail.com>
2020-10-05 17:20:05 +0100

    Updates

---
+++ precache-manifest.367bc0f9b99e4109b8aea2f625acad8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f30615a0d57ff92ca14575d0131ca6",
+    "url": "/Robofriends-App/index.html"
+  },
+  {
+    "revision": "b0e1e2713745c676ea95",
+    "url": "/Robofriends-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "01fac62c06cb5a2376fe",
+    "url": "/Robofriends-App/static/css/main.b60aac09.chunk.css"
+  },
+  {
+    "revision": "b0e1e2713745c676ea95",
+    "url": "/Robofriends-App/static/js/2.bef8b9ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends-App/static/js/2.bef8b9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01fac62c06cb5a2376fe",
+    "url": "/Robofriends-App/static/js/main.9c8cf800.chunk.js"
+  },
+  {
+    "revision": "612834b8959b2bd34aeb",
+    "url": "/Robofriends-App/static/js/runtime-main.d38b3dc4.js"
+  }
+]);
